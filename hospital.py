a723ed2006b55f586b5f600818d1e54fa6243ab0
deme <demeuforia@gmail.com>
2025-11-12 12:37:02 +0100

    incompleto

--- hospital.py
+++ hospital.py
@@ -7,37 +7,45 @@ app = FastAPI()
 # Lista para almacenar los pacientes
 pacientes_db = []
 
+contador_id = 0
+
 class Contacto(BaseModel):
     telefono: str
     email: Optional[str] = None
     
 class Paciente(BaseModel):
+    id: int
     nombre: str
     apellido: str
     edad: int
     contacto: Contacto
-    """
     alergias: Optional[str] = []
     activo: Optional[bool] = True
-    """    
+       
     
 # registra un nuevo paciente
 @app.post("/pacientes")
 async def crear_paciente(paciente: Paciente):
     # Guardar el libro en la lista
+    global contador_id
+    contador_id += 1 
     pacientes_db.append(paciente.model_dump())
     
     return {
         "mensaje": f"Se ha creado el paciente {paciente.nombre} con éxito",
         "datos": {
+            "id": paciente.id,
             "nombre": paciente.nombre,
             "apellido": paciente.apellido,
             "edad": paciente.edad,
-            "contacto": paciente.contacto
+            "contacto": paciente.contacto,
+            "alergias": paciente.alergias,
+            "activo": paciente.activo,
             
         }
-    }
     
+    }
+   
     
             
 
@@ -45,19 +53,19 @@ async def crear_paciente(paciente: Paciente):
 @app.get("/pacientes")
 def ver_pacientes():
     return {
-        "total": len(pacientes_db),
-        "libros": pacientes_db
+        "pacientes": pacientes_db
     }
 
-"""
+
 
 # devuelve un paciente especifico    
-@app.get("/paciente/{paciente_id}")
-def ver_libros():
-    return {
-        "total": len(pacientes_db),
-        "libros": pacientes_db
-    }
+@app.get("/pacientes/{contador_id}")
+def ver_paciente(contador_id = int):
+    for i in pacientes_db:
+        if i["id"] == contador_id:
+            return i
+
+"""
 
 # devuelve solo pacientes activos
 @app.get("/pacientes/activos")
